0505438dbe3d8939b091b4e5f398b87cccc187cd
Yavuz Selim Karaağaç <147400043+yavuzskaraagac@users.noreply.github.com>
2024-11-20 19:15:57 +0300

    Add files via upload

---
+++ YazarVeKitap2/YazarVeKitap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YazarVeKitap
+{
+    class Yazar
+    {
+        public string Ad { get; set; }
+        public string Ulke { get; set; }
+        public List<Kitap> Kitaplar { get; set; } = new List<Kitap>();
+
+        public void KitapEkle(Kitap kitap)
+        {
+            Kitaplar.Add(kitap);
+            kitap.Yazar = this;
+        }
+    }
+
+    class Kitap
+    {
+        public string Baslik { get; set; }
+        public DateTime YayinTarihi { get; set; }
+        public Yazar Yazar { get; set; }
+
+        public void YazarAtama(Yazar yazar)
+        {
+            Yazar = yazar;
+            yazar.KitapEkle(this);
+        }
+    }
+}
